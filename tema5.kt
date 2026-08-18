23c580fbfe80ccc3ae5261272982e7115e39ead3
CarlosD-255 <b22030119@alumnos.itsur.edu.mx>
2026-08-18 01:16:39 -0600

    Update tema5.kt

--- tema5.kt
+++ tema5.kt
@@ -30,3 +30,22 @@ fun main() {
     println(intervalInSeconds(0, 10, 0))
     println(intervalInSeconds(1, 0, 1))
 }
+//5.4
+fun main() {
+    val actions = listOf("title", "year", "author")
+    val prefix = "https://example.com/book-info"
+    val id = 5
+    val urls = actions.map { action -> "$prefix/$id/$action" }
+    println(urls)
+}
+//5.5
+fun repeatN(n: Int, action: () -> Unit) {
+    for (i in 1..n) {
+        action()
+    }
+}
+fun main() {
+    repeatN(5) {
+        println("Hello")
+    }
+}
